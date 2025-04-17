ace1e5316f94756ed15ac772bf188fe80c8068e9
tangli <1522906704@qq.com>
2025-04-18 06:13:01 +0800

    f

--- yibazhan/js/my.js
+++ yibazhan/js/my.js
@@ -1,5 +1,6 @@
 $(function() {
     const baseUrl="http://47.116.185.113:8085";
+    const baseUrlApi="http://47.116.185.113:8085/api";
     //标题
     $.get(baseUrl+'/api/sysCategory/getNavigationTree',(res)=>{
         let {data}=res;
@@ -28,8 +29,8 @@ $(function() {
                         </div>
                 </li>`
         })
-        $('#nav').append(str2);
-        $('#nav2').append(str2);
+        // $('#nav').append(str2);
+        // $('#nav2').append(str);
         resizefs()
         Nav('#nav') //导航
         Nav2('#nav2')
@@ -140,6 +141,301 @@ $(function() {
             auto: false
         });
     })
+
+    //律师在线
+    $.get(baseUrl+'/api/person/webPage?page=1&size=2&type=37',(res)=>{
+        const { data } = res;
+        let str=``;
+        data.records.forEach(item=>{
+            str+=`
+                <div class="user-info">
+                    <img src='${baseUrlApi+item.profilePicture}' alt="值班咨询师" class="avatar">
+                    <div class="details">
+                        <p class="name">${item.name}</p>
+                    </div>
+                </div>`
+        })
+        $('#lvshi').append(str);
+    })
+
+    //心理咨询
+    $.get(baseUrl+'/api/person/webPage?page=1&size=2&type=38',(res)=>{
+        const { data } = res;
+        let str=``;
+        data.records.forEach(item=>{
+            str+=`
+                <div class="user-info">
+                    <img src='${baseUrlApi+item.profilePicture}' alt="值班咨询师" class="avatar">
+                    <div class="details">
+                        <p class="name">${item.name}</p>
+                    </div>
+                </div>`
+        })
+        $('#xinli').append(str);
+    })
+
+    //健康问诊
+    $.get(baseUrl+'/api/person/webPage?page=1&size=2&type=36',(res)=>{
+        const { data } = res;
+        let str=``;
+        data.records.forEach(item=>{
+            str+=`
+                <div class="user-info">
+                    <img src='${baseUrlApi+item.profilePicture}' alt="值班咨询师" class="avatar">
+                    <div class="details">
+                        <p class="name">${item.name}</p>
+                    </div>
+                </div>`
+        })
+        $('#jiankang').append(str);
+    })
+
+    //分类
+    $.get(baseUrl+'/api/article/StrongMilitaryInformation?limit=7',(res)=>{
+        const { data } = res;
+        let str1=``;
+        //健康问诊
+        data.jianKangWenZhenPages.records.forEach(item=>{
+            str1+=`
+                <div class="news-list">
+                    <span class="news-item">
+                        <span class="news-title">${item.title}</span>
+                        <span class="separator">|</span>
+                    </span>
+                </div>`;
+        })
+        $('#jiankangwenzhen').append(str1);
+        //律师在线
+        let str2=``;
+        data.lvShiZaiXianPages.records.forEach(item=>{
+            str2+=`
+                <div class="news-list">
+                    <span class="news-item">
+                        <span class="news-title">${item.title}</span>
+                        <span class="separator">|</span>
+                    </span>
+                </div>`;
+        })
+        $('#lvshizaixian').append(str2);
+        //心里咨询
+        let str3=``;
+        data.xinLingYiZhanPages.records.forEach(item=>{
+            str3+=`
+                <div class="news-list">
+                    <span class="news-item">
+                        <span class="news-title">${item.title}</span>
+                        <span class="separator">|</span>
+                    </span>
+                </div>`;
+        })
+        $('#xinlizixun').append(str3);
+    })
+
+    //人物风采
+    $.get(baseUrl+'/api/article/page?page=1&size=50&cid=130',(res)=>{
+        const { data } = res;
+        console.log(1111111,data)
+        let str=``;
+        data.records.forEach(item=>{
+            str+=`<li class="swiper-slide">
+                <a href="#" class="news_item">
+                    <div class="infoBox">
+                        <div class="name gp-f24">杨士莪</div>
+                        <div class="subtitle gp-f20">${item.title}</div>
+                        <div class="summary gp-f16 gp-ellipsis-2">
+                            ${item.content}
+                        </div>
+                    </div>
+                    <div class="img"><img src="${baseUrlApi+item.pic}" alt=""></div>
+                </a>
+            </li>`
+        })
+        // data.records.forEach(item=>{
+        //     str+=`
+        //         <li class="swiper-slide">
+        //             <a href="https://www.nudt.edu.cn/rwfc/82e3c0c2b769478fb6ddf66888882310.htm" class="news_item">
+        //                 <div class="infoBox">
+        //                     <div class="name gp-f24">唐小妹</div>
+        //                     <div class="subtitle gp-f20">国防科大研究员唐小妹被授予全国三八红旗手称号</div>
+        //                     <div class="summary gp-f16 gp-ellipsis-2">
+        //                         为表彰先进、树立榜样，激励广大妇女自尊自信、自立自强，奋进新征程、建功新时代，积极投身中国式现代化建设，全国妇联决定，授予王志勤等10人全国三八红旗手标兵称号，授予张晓欢等596人全国三八红旗手称号，授予首都医科大学附属北京妇产医院北京妇幼保健院等395个单位全国三八红旗集体称号，授予北京市城建园林金都公司绿化三大队天安门广场特养班组等599个单位全国巾帼文明岗称号。其中，国防科技大学电子科学学院研究员唐小妹被授予全国三八红旗手称号。
+        //                     </div>
+        //                 </div>
+        //                 <div class="img"><img src="yibazhan/images/c265e435f9ea456495bb75bc4cf10ca4.jpg" alt=""></div>
+        //             </a>
+        //         </li>`;
+        // })
+        $('#renwufengcai').append(str);
+        var bg = document.querySelector('.item-bg');
+var items = document.querySelectorAll('.news_item');
+var item = document.querySelector('.news_item');
+var mode05Left = document.getElementsByClassName('mode05_slides')[0].getBoundingClientRect().left;
+
+function cLog(content) {
+    console.log(content)
+}
+$(function () {
+    var aHeight = [];
+    $('.news_item .infoBox').each(function (index, element) {
+        aHeight.push($(this).innerHeight())
+    });
+    // $('.news_item .infoBox').css('height',Math.max(...aHeight));
+    $('.news_item .infoBox').css('height',Math.max.apply(null, aHeight));
+})
+
+if($(window).width() > 800) {
+    $('.wrap_mode05').on("mouseover", ".news_item", function (_event, _element) {
+        var x = $(this).offset().left - mode05Left;
+        var y = $(this).offset().top;
+        var width = $(this).outerWidth();
+        var height = $(this).outerHeight();
+
+        $('.item-bg').addClass('active');
+        $('.news_item').removeClass('active');
+        $(this).addClass('active');
+        // $('.news_item').removeClass('active');
+
+        bg.style.width = width + 'px';
+        bg.style.height = height + 'px';
+        bg.style.transform = 'translateX(' + x + 'px )';
+    });
+    $('.wrap_mode05').on("mouseleave", ".news_item", function (_event, _element) {
+        $('.item-bg').removeClass('active');
+        $('.news_item').removeClass('active');
+    })
+
+    $(window).resize(function () {
+        if( $('.news_item.active').length > 0 ) {
+            mode05Left = document.getElementsByClassName('mode05_slides')[0].getBoundingClientRect().left;
+            var _this = $('.news_item.active');
+            var x = $(_this).offset().left - mode05Left;
+            var y = $(_this).offset().top;
+            var width = $(_this).outerWidth();
+            var height = $(_this).outerHeight();
+
+            bg.style.transform = 'translateX(' + x + 'px )';
+            bg.style.width = width + 'px';
+            bg.style.height = height + 'px';
+        }
+    });
+    $('.body_b').scroll(function () {
+        if( $('.news_item.active').length > 0 ){
+            mode05Left = document.getElementsByClassName('mode05_slides')[0].getBoundingClientRect().left;
+            var _this = $('.news_item.active');
+            var x = $(_this).offset().left - mode05Left;
+            var y = $(_this).offset().top;
+            var width = $(_this).outerWidth();
+            var height = $(_this).outerHeight();
+    
+            bg.style.transform = 'translateX(' + x + 'px )';
+            bg.style.width = width + 'px';
+            bg.style.height = height + 'px';
+        }
+    });
+}
+
+var mode05_slides = new Swiper('.mode05_slides', {
+    effect: 'coverflow',
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    keyboard: true,
+    slidesPerView: 3,
+    spaceBetween: 45,
+    speed: 300,
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false
+    // },
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 3,
+        slideShadows: false
+    },
+    breakpoints: {
+        998: {
+            spaceBetween: 45,
+            slidesPerView: 3,
+            centeredSlides: true
+        },
+        320: {
+            spaceBetween: 45,
+            slidesPerView: 1,
+            centeredSlides: true
+        }
+    },
+    simulateTouch: true,
+    navigation: {
+        nextEl: '.news-slider-next',
+        prevEl: '.news-slider-prev'
+    },
+    pagination: {
+        el: '.news-slider__pagination',
+        clickable: true,
+        type : 'progressbar',
+    },
+    on: {
+        init: function () {
+            var activeItem = document.getElementsByClassName('mode05_slides')[0].querySelector('.swiper-slide-active');
+            var sliderItem = activeItem.querySelector('.news_item');
+            $('.swiper-slide-active .news_item').addClass('active');
+            var x = sliderItem.getBoundingClientRect().left - mode05Left;
+            var y = sliderItem.getBoundingClientRect().top;
+            var width = sliderItem.getBoundingClientRect().width;
+            var height = sliderItem.getBoundingClientRect().height;
+            $('.item-bg').addClass('active');
+            bg.style.width = width + 'px';
+            bg.style.height = height + 'px';
+            bg.style.transform = 'translateX(' + x + 'px )';
+            // bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
+        }
+    }
 });
 
+mode05_slides.on('touchEnd', function () {
+    $('.news_item').removeClass('active');
+    $('.swiper-slide-active .news_item').addClass('active');
+});
+
+mode05_slides.on('slideChange', function () {
+    $('.news_item').removeClass('active');
+});
+
+mode05_slides.on('slideChangeTransitionEnd', function () {
+    $('.news_item').removeClass('active');
+    var activeItem = document.querySelector('.swiper-slide-active');
+    var sliderItem = activeItem.querySelector('.news_item');
+    $('.swiper-slide-active .news_item').addClass('active');
+    var x = document.querySelector('.swiper-slide-active .news_item').getBoundingClientRect().left - mode05Left;
+    var y = document.querySelector('.swiper-slide-active .news_item').getBoundingClientRect().top;
+    /* var width = sliderItem.getBoundingClientRect().width;
+    var height = sliderItem.getBoundingClientRect().height; */
+    $('.item-bg').addClass('active');
+    /* bg.style.width = width + 'px';
+    bg.style.height = height + 'px'; */
+    bg.style.transform = 'translateX(' + x + 'px )';
+    // bg.style.transform = 'translateX(' + x + 'px ) translateY(' + y + 'px)';
+});
+
+    })
+
+    //祁连艺苑
+    $.get(baseUrl+'/api/friendshipLink/webArtList',(res)=>{
+        const { data } = res;
+        //祁连艺术
+        let str1=`<a href="#" class="gp-img-responsive" style="background-image: url(${baseUrlApi+data.theaterList[0].icon});background-size: 100% 100%;background-position: center;padding-bottom: 0px;height: 100%;"></a>`;
+        $('#yishu').append(str1);
+        //祁连画坛
+        let str2=`<a href="#" class="gp-img-responsive" style="background-image: url(${baseUrlApi+data.otherList[0].icon});background-size: 100% 100%;background-position: center;padding-bottom: 0px;height: 100%;"></a>`;
+        $('#huatan').append(str2);
+        //祁连音乐
+        let str3=`<a href="#" class="gp-img-responsive" style="background-image: url(${baseUrlApi+data.otherList[1].icon});background-position: center;background-size: cover;padding-bottom: 0px;height: 100%;"></a>`;
+        $('#yinyue').append(str3);
+        //祁连阅读
+        let str4=`<a href="#" class="gp-img-responsive" style="background-image: url(${baseUrlApi+data.otherList[2].icon});background-position: center;background-size: cover;padding-bottom: 0px;height: 100%;"></a>`;
+        $('#yuedu').append(str4);
+    })
+});
 
